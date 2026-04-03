3539434bc79fa36e4e51ccb62f1d05e7dd9ab435
dasdomin103 <dasdomin103@gmail.com>
2026-04-03 23:49:37 +0530

    homepage fixed and verified

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // Remove this line ❌
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
